5dd27f26f6bfdae373236515352b29fc5b93dff4
mrlw500 <marshalweir@icloud.com>
2022-06-12 15:50:22 +0100

    launch client window

--- src/main/java/com/posgames/spotwars/LandingPageController.java
+++ src/main/java/com/posgames/spotwars/LandingPageController.java
@@ -1,11 +1,29 @@
 package com.posgames.spotwars;
 
+import com.posgames.spotwars.gameclient.GameClient;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class LandingPageController {
 
+    @FXML private Button buttonLaunchGame;
+
     @FXML
     private void launchGame(){
-
+        Stage lpStage = (Stage) buttonLaunchGame.getScene().getWindow();
+        Stage clientStage = new Stage();
+        GameClient gameClient = new GameClient();
+        lpStage.hide();
+        try {
+            gameClient.start(clientStage);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        clientStage.showAndWait();
+        lpStage.show();
     }
 }
--- src/main/java/com/posgames/spotwars/gameclient/GameClient.java
+++ src/main/java/com/posgames/spotwars/gameclient/GameClient.java
@@ -1,14 +1,21 @@
 package com.posgames.spotwars.gameclient;
 
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class GameClient extends Application {
+    private Group root = new Group();
+    private Canvas canvas = new Canvas();
+
 
     @Override
-    public void start(Stage stage) {
-        stage.setTitle("Spot Wars");
-        stage.setFullScreen(true);
+    public void start(Stage stage) throws Exception {
+        Scene scene = new Scene(root,1920, 1080);
+        stage.setScene(scene);
+        stage.setTitle("Spot Wars 2");
+       // stage.show();
         //TODO Set mins
     }
 
